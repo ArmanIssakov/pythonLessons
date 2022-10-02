dabef9817762f65388da0fb7cf1d2b7f71b34ad3
Armanissakov <arman.isakov.96@mail.ru>
2022-10-02 22:23:09 +0600

    homework02 is done

---
+++ HomeWork02/ExHw03.py
@@ -0,0 +1,15 @@
+# 3. Задайте список из n чисел, заполненный по формуле 
+# (1 + 1/n) ** n и выведите на экран их сумму.
+
+n = int(input('Введите число:'))
+num = []
+sumA = 0
+for i in range(1,n+1):
+    a =round((1+1/i)**i)
+    num.append(a)
+    sumA +=a
+print(num)
+print()
+print(sumA)
+    
+    
